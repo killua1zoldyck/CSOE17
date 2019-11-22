74ba16bfb022148028ac5d01a531e9d55a2bd5e7
Greenleaf20 <archana.2098@gmail.com>
2019-11-23 04:49:14 +0530

    File to sort dates and modified main code to read from the sorted file

--- uberdataanalysis.py
+++ uberdataanalysis.py
@@ -6,31 +6,24 @@ import csv
 x = []
 y = []
 
-with open('april.txt','r') as csvfile:
+with open('testdate.txt','r') as csvfile:
     plots = csv.reader(csvfile, delimiter='\t')
+    print "HERE"
     for row in plots:
-        # print row[0]
+        print row[0]
         x.append(str(row[0]))
         y.append(float(row[1]))
 
-# dict = {new_list: [] for new_list in range(len(x))} 
-
-# for i in range(len(x)):
-#     dict[x[i]]=y[i]
-
-# dict.sort()
-# print dict
-# plt.plot(x,y, label='Loaded from file!')
 plt.xticks(rotation='vertical')
 plt.bar(x,y)
 plt.xlabel('Date in MM/DD/YY format')
 plt.ylabel('Frequency of rides')
-plt.title('Date vs Frequency plot of Uber Data Analysis\nApril')
+plt.title('Date vs Frequency plot of Uber Data Analysis\nSeptember')
 
-
+repr(x)
 plt.legend()
 plt.show()
 
-plt.savefig('april date graph.png',bbox_inches='tight')
+plt.savefig('September.png',bbox_inches='tight')
 
 print "END"
