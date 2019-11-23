371dae0dfaa87ea038dc1617a68b790230ffacc6
Greenleaf20 <archana.2098@gmail.com>
2019-11-24 00:26:47 +0530

    modified code with single read from file

--- uberdataanalysis.py
+++ uberdataanalysis.py
@@ -3,25 +3,69 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import csv
 
-x = []
-y = []
 
 def func(x): 
     a=x.split()
     b=a[0].split('/')
-    print b[1]
+    # print b[1]
     return int(b[1])
 
-f = open("september.txt","r")
+def func1(x):
+    a = x.split()
+    b = a[0].split('/')
+    # print b[0]
+    return b[0]
+
+def plot_func(x,y):
+    plt.clf()
+    plt.xticks(rotation='vertical')
+    plt.bar(x,y)
+    plt.xlabel('Date in MM/DD/YY format')
+    plt.ylabel('Frequency of rides')
+    plt.title('Date vs Frequency plot of Uber Data Analysis\n'+a[i])
+    plt.legend()
+    plt.show()
+    plt.savefig(a[i]+'test.png',bbox_inches='tight')
+
+a = ["April" , "May" , "June" , "July" , "August" , "September"]
+
+f = open("datevscount.txt","r")
 if f.mode == 'r':
     f1 = f.readlines()
 
 f1 = sorted(f1, key=func)
+f1 = sorted(f1, key=func1)
+
+i = 0
+month = a[i]
+m = 3  
 for line in f1:
+        if m==3:
+            x = []
+            y = []
+            m = m+1
+        if i>=6:
+            break
         row = line.split()
-        print row[0]
-        x.append(str(row[0]))
-        y.append(float(row[1]))
+        if int(m)!=int(line[0]):
+            plot_func(x,y)
+            print i
+            i=i+1
+            del x
+            del y
+            x = []
+            y = []
+            x.append(str(row[0]))
+            y.append(float(row[1]))
+        else:
+            x.append(str(row[0]))
+            y.append(float(row[1]))
+        m = line[0]
+
+print i
+plot_func(x,y)
+
+print "END"
 
 # with open('testdate.txt','r') as csvfile:
 #     plots = csv.reader(csvfile, delimiter='\t')
@@ -31,16 +75,5 @@ for line in f1:
 #         x.append(str(row[0]))
 #         y.append(float(row[1]))
 
-plt.xticks(rotation='vertical')
-plt.bar(x,y)
-plt.xlabel('Date in MM/DD/YY format')
-plt.ylabel('Frequency of rides')
-plt.title('Date vs Frequency plot of Uber Data Analysis\nSeptember')
 
-repr(x)
-plt.legend()
-plt.show()
 
-plt.savefig('test.png',bbox_inches='tight')
-
-print "END"
